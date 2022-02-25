3b011317dd03b8ce45fedc048f431474acfe7ec8
Doustalet Chloé <cdoustalet@gmail.com>
2022-02-25 17:06:34 +0100

    Remove single ad for planet-add

--- wordpress/wp-content/themes/earthbnbTheme/planet-add.php
+++ wordpress/wp-content/themes/earthbnbTheme/planet-add.php
@@ -8,6 +8,18 @@
 
 <h1>Louer un bien</h1>
 
-<p>Ajout du formulaire de location. Proposer l'upload de plusieurs photos et en définir une principale ?</p>
+<?php while(have_posts()) : the_post(); ?>
+    <div>
+        <img src="<?php the_post_thumbnail_url() ; ?> alt="image annonce">
+    </div>
+    <div>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+        <p>Localisation : <?php get_post_meta(get_the_ID(), 'ad_localisation', true); ?>€</p>
+        <p>Superficie : <?php get_post_meta(get_the_ID(), 'ad_surface', true); ?>€</p>
+        <p>Prix : <?php get_post_meta(get_the_ID(), 'ad_price', true); ?>€</p>
+        <a href="<?php the_permalink(); ?>"> Acheter </a>
+    </div>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
