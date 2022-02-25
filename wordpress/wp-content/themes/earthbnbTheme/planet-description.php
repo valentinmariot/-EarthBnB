ef5aaf9a08bb704e6386d4f492236987d84233e6
Doustalet Chloé <78727498+chloe-dst@users.noreply.github.com>
2022-02-25 17:24:21 +0100

    Update planet-description.php

--- wordpress/wp-content/themes/earthbnbTheme/planet-description.php
+++ wordpress/wp-content/themes/earthbnbTheme/planet-description.php
@@ -8,13 +8,18 @@
 
 <h1>Planet description template</h1>
 
- <?php 
-    if (have_posts()) : 
-        while (have_posts()): 
-            the_post();
-?>
-
-
+<?php while(have_posts()) : the_post(); ?>
+    <div>
+        <img src="<?php the_post_thumbnail_url() ; ?> alt="image annonce">
+    </div>
+    <div>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+        <p>Localisation : <?php get_post_meta(get_the_ID(), 'ad_localisation', true); ?>€</p>
+        <p>Superficie : <?php get_post_meta(get_the_ID(), 'ad_surface', true); ?>€</p>
+        <p>Prix : <?php get_post_meta(get_the_ID(), 'ad_price', true); ?>€</p>
+        <a href="<?php the_permalink(); ?>"> Acheter </a>
+    </div>
 <?php endwhile; ?>
-<?php endif;?>
+                                           
 <?php get_footer(); ?>
