51bb1d1f3a140a20c20e06da353808b106c24b2d
Doustalet Chloé <cdoustalet@gmail.com>
2022-02-25 11:03:23 +0100

    Login first steps
    - Handle redirection after login according to user profile
    - Don't show admin bar for customers
    - Login page creation

--- wordpress/wp-content/themes/earthbnbTheme/functions.php
+++ wordpress/wp-content/themes/earthbnbTheme/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+#theme setup
 add_action('after_setup_theme', 'EarthbnbTheme');
 function EarthbnbTheme()
 {
@@ -7,4 +8,26 @@ function EarthbnbTheme()
     add_theme_support('post-thumbnails');
     add_theme_support('menus');
     register_nav_menu('header', 'Le menu du header mec');
+}
+
+add_action('after_setup_theme', 'remove_Admin_bar');
+
+#login management
+function wpdocs_my_login_redirect( $url, $request, $user ) {
+    if ( $user && is_object( $user ) && is_a( $user, 'WP_User' ) ) {
+        if ( $user->has_cap( 'administrator' || 'manage_ad') ) {
+            $url = admin_url();
+        } else {
+            $url = home_url( '/account/' );
+        }
+    }
+    return $url;
+}
+
+add_filter( 'login_redirect', 'wpdocs_my_login_redirect', 10, 3 );
+
+function remove_admin_bar(){
+    if (!current_user_can('manage_options') ) {
+        add_filter('show_admin_bar', '__return_false');
+    }
 }
