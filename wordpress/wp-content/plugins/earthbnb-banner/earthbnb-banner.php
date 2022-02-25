34bfa3209ff2d49c5721b83930d064ef17b1b788
Ada Loveless <xhorj@protonmail.com>
2022-02-25 17:39:31 +0100

    feat: implementing plugin class activation

--- wordpress/wp-content/plugins/earthbnb-banner/earthbnb-banner.php
+++ wordpress/wp-content/plugins/earthbnb-banner/earthbnb-banner.php
@@ -1,7 +1,40 @@
 <?php
+/**
+ * @package EarthBnBBanner
+ */
 /*
 Plugin Name: EarthBnB Banner
 Description: Une bannière pour EarthBnb
 Version: 1.0.0
 Author: EarthBnb
 */
+
+# security
+defined( 'ABSPATH' ) or die( 'Hey, you can\t access this file, you silly human!' );
+
+class BannerPlugin
+{
+    function activate() {
+        // generate a CPT
+        // flush rewrite rules
+    }
+
+    function deactivate() {
+        // flush rewrite rules
+
+    }
+
+    function uninstall() {
+        // delete CPT
+    }
+
+}
+if ( class_exists( 'BannerPluggin' ) ) {
+    $bannerPluggin = new BannerPlugin();
+}
+
+//activation
+register_activation_hook( __FILE__, array($bannerPluggin, 'activate') );
+
+//deactivation
+register_deactivation_hook( __FILE__, array($bannerPluggin, 'deactivate') );
