ed2c0cdf2d3f475ca9e45d294acfc92d18c38d7e
Doustalet Chloé <78727498+chloe-dst@users.noreply.github.com>
2022-02-25 17:22:52 +0100

    Update planet-add.php

--- wordpress/wp-content/themes/earthbnbTheme/planet-add.php
+++ wordpress/wp-content/themes/earthbnbTheme/planet-add.php
@@ -8,18 +8,6 @@
 
 <h1>Louer un bien</h1>
 
-<?php while(have_posts()) : the_post(); ?>
-    <div>
-        <img src="<?php the_post_thumbnail_url() ; ?> alt="image annonce">
-    </div>
-    <div>
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
-        <p>Localisation : <?php get_post_meta(get_the_ID(), 'ad_localisation', true); ?>€</p>
-        <p>Superficie : <?php get_post_meta(get_the_ID(), 'ad_surface', true); ?>€</p>
-        <p>Prix : <?php get_post_meta(get_the_ID(), 'ad_price', true); ?>€</p>
-        <a href="<?php the_permalink(); ?>"> Acheter </a>
-    </div>
-<?php endwhile; ?>
+<p>Formulaire pour louer son bien</p>
 
 <?php get_footer(); ?>
