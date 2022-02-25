8f92b45e0789b0aa5cfe3424048cd76cfd949816
Ada Loveless <xhorj@protonmail.com>
2022-02-25 20:19:41 +0100

    feat: adding banner custom post type (not working)

--- wordpress/wp-content/plugins/earthbnb-banner/earthbnb-banner.php
+++ wordpress/wp-content/plugins/earthbnb-banner/earthbnb-banner.php
@@ -16,6 +16,7 @@ class BannerPlugin
 {
     function activate() {
         // generate a CPT
+        $this->banners_post_type();
         // flush rewrite rules
         flush_rewrite_rules();
     }
@@ -25,10 +26,39 @@ class BannerPlugin
         flush_rewrite_rules();
     }
 
+    // register a custom post type called 'banner'
+    function banners_post_type() {
+        $labels = array(
+            'name' => __( 'Banners' ),
+            'singular_name' => __( 'banner' ),
+            'add_new' => __( 'New banner' ),
+            'add_new_item' => __( 'Add New banner' ),
+            'edit_item' => __( 'Edit banner' ),
+            'new_item' => __( 'New banner' ),
+            'view_item' => __( 'View banner' ),
+            'search_items' => __( 'Search banners' ),
+            'not_found' =>  __( 'No banners Found' ),
+            'not_found_in_trash' => __( 'No banners found in Trash' ),
+        );
+        $args = array(
+            'labels' => $labels,
+            'has_archive' => true,
+            'public' => true,
+            'show_in_menu' => true,
+            'show_ui' => true,
+            'hierarchical' => false,
+            'supports' => array(
+                'title',
+                'editor',
+                'excerpt',
+                'custom-fields',
+                'thumbnail',
+                'page-attributes'
+            ),
+            'taxonomies' => array( 'post_tag', 'category'),
+        );
+        register_post_type( 'banner', $args );
     }
-
-    function uninstall() {
-        // delete CPT
     }
 
 }
