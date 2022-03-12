edc64b641eb6548dfe1879a272fec89b89623922
valentin Mariot <valentinmariot@gmail.com>
2022-03-13 00:37:38 +0100

    home form v1

--- wordpress/wp-content/themes/earthbnbTheme/header.php
+++ wordpress/wp-content/themes/earthbnbTheme/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); wp_title(); ?></title>
-    <link href="https://kit-pro.fontawesome.com/releases/v5.15.3/css/pro.min.css" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/a373cfe327.js" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 
@@ -18,6 +18,3 @@
         <header>
             <?php wp_nav_menu(array('theme_location'=>'header')); ?>
         </header>
-   
-        <!-- // @NOTE : TODO -> remove <hr> -->
-        <hr/>
