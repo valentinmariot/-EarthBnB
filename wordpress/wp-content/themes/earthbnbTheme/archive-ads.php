733b104d7b95a2d312f6569d072ef2121095a943
Doustalet Chloé <78727498+chloe-dst@users.noreply.github.com>
2022-03-13 23:13:38 +0100

    Update archive-ads.php

--- wordpress/wp-content/themes/earthbnbTheme/archive-ads.php
+++ wordpress/wp-content/themes/earthbnbTheme/archive-ads.php
@@ -5,6 +5,8 @@
 ?>
 
 <?php get_header(); ?>
+<!--
+POUR LES FILTRES MAIS PAS DU TOUT FONCTIONNEL
     <span>Distance</span>
     <ul>
         <li><a href="#">+ de 20 parsecs</a></li>
@@ -20,28 +22,23 @@
         <li><a href="#">- de 100 euros</a></li>
         <li><a href="#">- de 50 euros</a></li>
     </ul>
+ -->
 
-<?php
-    echo wp_list_categories();
-?>
-
-<?php wp_adds(); ?>
-
-<!-- J'ai plutôt utilisé une query au final mais je voulais verifier avec vous. Je me demande par exemple s'il ne faut pas rajouter le if (have_posts()) avant d'appeler la query
 <?php if (have_posts()) : ?>
     <div>
         <?php while (have_posts()): ?>
             <?php the_post(); ?>
             <div>
-                <img src="<?php the_post_thumbnail_url() ; ?> alt="image annonce">
-                <div>
-                    <h3><?php the_title(); ?></h3>
-                </div>
+                <?php the_post_thumbnail(); ?>
+                <li><a href="<?php get_the_permalink(); ?>" rel="bookmark"> <?= get_the_title() ; ?></a></li>
+                <p>Prix : <?= get_post_meta(get_the_ID(), 'ad_price', true); ?>€</p>
+                <p>Distance : <?= get_post_meta(get_the_ID(), 'ad_localisation', true); ?> parsecs</p>
+                <?= get_the_excerpt(); ?>
+                <br><a href="<?php get_permalink(); ?>">Détails </a><br><br>
             </div>
         <?php endwhile; ?>
+        <?= wpheticPaginate(); ?>
     </div>
-<?php endif;?> -->
-
-<p>Manque la pagination </p>
+<?php endif;?>
 
 <?php get_footer(); ?>
