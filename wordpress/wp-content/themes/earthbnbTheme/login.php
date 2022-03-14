f9b3f41c27b292149b4f3f8284f308623a7e655c
DFarau <doriane.farau@hetic.net>
2022-03-14 15:01:07 +0100

    form register and login

--- wordpress/wp-content/themes/earthbnbTheme/login.php
+++ wordpress/wp-content/themes/earthbnbTheme/login.php
@@ -3,15 +3,16 @@
  * Template Name: login
  */
 
-if (is_user_logged_in()) {
+/*if (is_user_logged_in()) {
     ob_start();
     nocache_headers();
     $url = home_url("/mon-compte/");
     wp_redirect($url);
     exit;
-}else{
-    get_header();
-    echo wp_login_form();
-};
+}else{*/
+    get_header(); ?>
+    <h2 class="form_title">Connexion</h2>
+    <?php echo wp_login_form();
+//};
 
 get_footer();
