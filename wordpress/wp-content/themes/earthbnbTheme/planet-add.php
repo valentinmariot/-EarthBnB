bbcdb20c500b22f3e04ae459814c4d52bd322139
Ada Loveless <xhorj@protonmail.com>
2022-03-14 13:41:37 +0100

    feat: creating style for planet add form

--- wordpress/wp-content/themes/earthbnbTheme/planet-add.php
+++ wordpress/wp-content/themes/earthbnbTheme/planet-add.php
@@ -10,19 +10,19 @@
 
 <p>Formulaire pour louer son bien</p> 
 
-<form method="post">
-<label for="planet_name">Nom du lieu:</label> <input type="text" name="planet_name" value="<?php echo $planet_name;?>">
-<label for="ad_price">Coût:</label> <input type="text" name="ad_price" value="<?php echo $price;?>">
-<label for="ad_distance">Distance:</label> <input type="number" name="ad_distance" value="<?php echo $distance;?>">
-<label for="ad_surface">Surface:</label> <input type="number" name="ad_surface" value="<?php echo $surface;?>">
-<label for="description">Description:</label> <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
-<input type="file" id='gallery' name="pictures[]" multiple>
-<p>
+<form class="planet-add-form" method="post">
+<section class="planet-add-form__field"><label class="planet-add-form__label" for="planet_name">Nom du lieu:</label> <input class="planet-add-form__input" type="text" name="planet_name" value="<?php echo $planet_name;?>"></section>
+<section class="planet-add-form__field"><label class="planet-add-form__label" for="ad_price">Coût:</label> <input class="planet-add-form__input" type="text" name="ad_price" value="<?php echo $price;?>"></section>
+<section class="planet-add-form__field"><label class="planet-add-form__label" for="ad_distance">Distance:</label> <input class="planet-add-form__input" type="number" name="ad_distance" value="<?php echo $distance;?>"></section>
+<section class="planet-add-form__field"><label class="planet-add-form__label" for="ad_surface">Surface:</label> <input class="planet-add-form__input" type="number" name="ad_surface" value="<?php echo $surface;?>"></section>
+<section class="planet-add-form__field"><label class="planet-add-form__label" for="description">Description:</label> <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea></section>
+<!-- <input type="file" id='gallery' name="pictures[]" multiple> -->
+<section class="planet-add-form__field">
     <?php wp_dropdown_categories( 'show_option_none=Météo&tab_index=4&taxonomy=weather' ); ?>
     <?php wp_dropdown_categories( 'show_option_none=Activités&tab_index=4&taxonomy=price' ); ?>
-</p>
+</section>
 <input type="hidden" name="post_type" value="ads" />
-<button type="submit">Publier</button>
+<section class="planet-add-form__field"><button type="submit">Publier</button></section>
 </form>
 
 <?php get_footer(); ?>
